fa7c0c4ac7ee6a8710ae7d911c73a72e3f8b0542
lhwisdom1 <49606445+lhwisdom1@users.noreply.github.com>
2021-06-13 18:44:51 -0500

    Add files via upload

---
+++ script.php
@@ -0,0 +1,9 @@
+<?php
+$First=8;//first
+$Second=4;//second
+echo "The sum of First and Second is : ". ($First+$Second) ."<br />";//addition operator
+echo "The difference between First and Second is : ". ($First-$Second) ."<br />";//Substraction operator
+echo "Multiplication of First and Second : ". ($First*$Second) ."<br />";//Multiplication operator
+echo "Division of First and Second : ". ($First/$Second) ."<br />";//Division operator
+echo "Modulus of First and Second : " . ($First%$Second) ."<br />";//Module operator
+?>
